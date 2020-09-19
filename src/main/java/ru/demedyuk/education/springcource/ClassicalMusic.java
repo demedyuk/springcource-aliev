52073337f2531b5c9c7e430ea158694f359e94c8
Andrey Demedyuk <demedyuk@ngs.ru>
2020-09-20 00:57:43 +0700

    Lesson 8.2: factory method

--- src/main/java/ru/demedyuk/education/springcource/ClassicalMusic.java
+++ src/main/java/ru/demedyuk/education/springcource/ClassicalMusic.java
@@ -2,6 +2,13 @@ package ru.demedyuk.education.springcource;
 
 public class ClassicalMusic implements Music {
 
+    private ClassicalMusic() {
+    }
+
+    public static ClassicalMusic getClassicalMusic() {
+        return new ClassicalMusic();
+    }
+
     @Override
     public String getSong() {
         return "HungarianRhapsody";
