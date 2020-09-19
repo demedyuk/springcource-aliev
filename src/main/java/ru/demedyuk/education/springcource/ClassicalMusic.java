acf93635a07bd0f54f0299538cda5da633096a76
Andrey Demedyuk <demedyuk@ngs.ru>
2020-09-20 00:50:32 +0700

    Lesson 8.1: init and destroy methods

--- src/main/java/ru/demedyuk/education/springcource/ClassicalMusic.java
+++ src/main/java/ru/demedyuk/education/springcource/ClassicalMusic.java
@@ -6,4 +6,11 @@ public class ClassicalMusic implements Music {
     public String getSong() {
         return "HungarianRhapsody";
     }
+
+    public void doMyInit() {
+        System.out.println("Do my initiatization");
+    }
+    public void doMyDestroy() {
+        System.out.println("Do my destruction");
+    }
 }
--- src/main/java/ru/demedyuk/education/springcource/TestSpring.java
+++ src/main/java/ru/demedyuk/education/springcource/TestSpring.java
@@ -6,19 +6,22 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-//        Music music = context.getBean("musicBean", Music.class);
+        ClassicalMusic classicalMusic = context.getBean("musicClassicalBean", ClassicalMusic.class);
+        System.out.println(classicalMusic.getSong());
+
+
 //        MusicPlayer musicPlayer = new MusicPlayer(music);
 
-        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-
-        firstMusicPlayer.setVolume(10);
-
-        boolean comparsion = firstMusicPlayer == secondMusicPlayer;
-        System.out.println(comparsion);
-
-        System.out.println("vol1: " + firstMusicPlayer.getVolume());
-        System.out.println("vol2: " + secondMusicPlayer.getVolume());
+//        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//
+//        firstMusicPlayer.setVolume(10);
+//
+//        boolean comparsion = firstMusicPlayer == secondMusicPlayer;
+//        System.out.println(comparsion);
+//
+//        System.out.println("vol1: " + firstMusicPlayer.getVolume());
+//        System.out.println("vol2: " + secondMusicPlayer.getVolume());
 
 //        System.out.println(musicPlayer.getName());
 //        System.out.println(musicPlayer.getVolume());
